822eaf8d33937c73d7671c86ce3aeedd694d5449
Geng Sun <gsun@iastate.edu>
2019-11-30 21:53:38 -0600

    Game basics works!!!

--- app/src/main/java/edu/iastate/cpre388_small_project/MainActivity.java
+++ app/src/main/java/edu/iastate/cpre388_small_project/MainActivity.java
@@ -1,14 +1,19 @@
 package edu.iastate.cpre388_small_project;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.graphics.drawable.AnimatedImageDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -58,34 +63,119 @@ public class MainActivity extends AppCompatActivity {
                 time_left.setText("Time Left: " + left);
                 user_score = 0;
                 score.setText("Score: ");
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startButton();
+                    }
+                },1000);
+                button.setEnabled(false);
             }
         });
 
         mole1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                tempifleft = 1;
+                mole1.setImageResource(R.drawable.img_sign_in_whack_mole_rat_normal_18);
+                user_score += 1;
+                score.setText("Score: " + user_score);
+                mole1.setEnabled(false);
             }
         });
 
         mole2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                tempifleft = 1;
+                mole2.setImageResource(R.drawable.img_sign_in_whack_mole_rat_normal_18);
+                user_score += 1;
+                score.setText("Score: " + user_score);
+                mole2.setEnabled(false);
             }
         });
 
         mole3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                tempifleft = 1;
+                mole3.setImageResource(R.drawable.img_sign_in_whack_mole_rat_normal_18);
+                user_score += 1;
+                score.setText("Score: " + user_score);
+                mole3.setEnabled(false);
             }
         });
 
         mole4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                tempifleft = 1;
+                mole4.setImageResource(R.drawable.img_sign_in_whack_mole_rat_normal_18);
+                user_score += 1;
+                score.setText("Score: " + user_score);
+                mole4.setEnabled(false);
             }
         });
+    }
+
+    private void startButton(){
+        do{
+            which = random.nextInt(4)+1;
+        } while (whichsave == which);
+        whichsave = which;
+
+        switch (which){
+            case 1:
+                mole1.setImageResource( R.drawable.img_sign_in_whack_mole_rat_normal_18);
+                mole1.setVisibility(View.VISIBLE);
+                mole4.setEnabled(true);
+                break;
+            case 2:
+                mole2.setImageResource( R.drawable.img_sign_in_whack_mole_rat_normal_18);
+                mole2.setVisibility(View.VISIBLE);
+                mole2.setEnabled(true);
+                break;
+            case 3:
+                mole3.setImageResource( R.drawable.img_sign_in_whack_mole_rat_normal_18);
+                mole3.setVisibility(View.VISIBLE);
+                mole3.setEnabled(true);
+                break;
+            case 4:
+                mole4.setImageResource( R.drawable.img_sign_in_whack_mole_rat_normal_18);
+                mole4.setVisibility(View.VISIBLE);
+                mole4.setEnabled(true);
+                break;
+        }
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mole1.setVisibility(View.INVISIBLE);
+                mole2.setVisibility(View.INVISIBLE);
+                mole3.setVisibility(View.INVISIBLE);
+                mole4.setVisibility(View.INVISIBLE);
+
+                mole1.setEnabled(false);
+                mole2.setEnabled(false);
+                mole3.setEnabled(false);
+                mole4.setEnabled(false);
+
+                if(tempifleft == 0){
+                    left = left-1;
+                    time_left.setText("Time Left: " + left);
+                }else if(tempifleft == 1){
+                    tempifleft = 0;
+                }
+
+                if(left==0){
+                    Toast.makeText(MainActivity.this, "Game Over", Toast.LENGTH_SHORT).show();
+                    button.setEnabled(true);
+                }else if (left > 0 ){
+                    startButton();
+                }
+            }
+        },fps);
     }
 }
